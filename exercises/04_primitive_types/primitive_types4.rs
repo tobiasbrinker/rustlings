f169c8d2c43c6b48e5b2fe738e4c3f21b2797afa
Tobias Brinker <tobias.brinker@yahoo.de>
2024-08-04 13:39:38 +0200

    save

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,8 @@
 fn main() {
     // You can optionally experiment here.
+    let a = [1, 2, 3, 4, 5];
+    let nice_slice  = &a[1..4];
+    dbg!(nice_slice);
 }
 
 #[cfg(test)]
@@ -9,7 +12,7 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice  = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
